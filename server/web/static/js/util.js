113c560f4cc2cabde44612899914500a4f5c30bd
liuchengtao <liuchengtao1982@gmail.com>
2012-10-17 16:43:21 +0800

    liuchengtao: add the progress icon for ajax request

--- server/web/static/js/util.js
+++ server/web/static/js/util.js
@@ -1,8 +1,72 @@
 var WebServerURL = 'http://192.168.7.212:8080';
 var SocketURL = "ws://192.168.7.212:8082";
 
-function invokeWebApi(cmd, jdata, call){
-    $.getJSON(WebServerURL+cmd+"?callback=?", jdata, call);
+//function invokeWebApi(cmd, jdata, call){
+//    $.getJSON(WebServerURL+cmd+"?callback=?", jdata, call);
+//}
+var ajaxstart=function() {
+		var winWidth=0;
+		var winHeight=0;
+		//获取窗口宽度	
+	    if(window.innerWidth) winWidth = window.innerWidth;	
+	    else if((document.body) && (document.body.clientWidth))	winWidth = document.body.clientWidth;
+	
+	    //获取窗口高度
+	    if(window.innerHeight) winHeight = window.innerHeight;	
+	    else if((document.body) && (document.body.clientHeight)) winHeight = document.body.clientHeight;
+	
+	    //通过深入Document内部对body进行检测，获取窗口大小
+	    if(document.documentElement && document.documentElement.clientHeight && document.documentElement.clientWidth) {
+		   winHeight = document.documentElement.clientHeight;
+		   winWidth = document.documentElement.clientWidth;
+	    }
+
+        if(document.getElementById('img') !== undefined && document.getElementById('img') !== null ) {
+	        document.getElementById('img').style.left=""+(winWidth/2-70)+"px";
+	        document.getElementById('img').style.top=""+(winHeight/2)+"px";
+	        document.getElementById('img').innerHTML = "<a><img style='BORDER:none' src='static/img/loading.gif'></a>";   	
+        }	    		    
+}; 
+
+var ajaxend = function(){
+	if(document.getElementById('img') !== undefined && document.getElementById('img') !== null )
+        document.getElementById('img').innerHTML = '';
+};
+
+/*
+ * Http Get Request by Jquery Ajax
+ */
+var invokeWebApi = function(apiUrl,dataj,render) {
+   
+     //ajax options prepare
+    var options = {}; 
+
+    var funok=function(data) {
+        if(data['results'] === undefined) {
+           alert("Server Internal error!");
+           ajaxend();
+        } else {
+           render(data);
+           ajaxend();          
+        }
+    };
+	
+	var funerror=function() {
+        alert("Server Internal error!");
+        ajaxend();
+    };
+
+    options['beforeSend'] = ajaxstart;
+    options['url'] = WebServerURL + apiUrl;	
+    options['async'] = true;
+    options['type'] = 'GET';
+    options['dataType'] = 'jsonp';
+    options['timeout'] = 15000;
+	options['success'] = funok;
+	options['error'] = funerror;
+			
+    //invoke jquery ajax
+    $.ajax(options);
 }
 
 function getWebsocket(subcmd){
