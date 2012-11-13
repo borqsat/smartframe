5e74b54eb24a0e856597863e23bb7bbd0e6981c1
liuchengtao <liuchengtao1982@gmail.com>
2012-11-13 13:39:54 +0800

    liuchengtao: use domain ats.borqs.com to replace ip for api server

--- server/web/static/js/util.js
+++ server/web/static/js/util.js
@@ -1,4 +1,4 @@
-var WebServerURL = 'http://192.168.7.212:8080';
+var WebServerURL = "http://ats.borqs.com/smartquery";
 var SocketURL = "ws://192.168.7.212:8082";
 //function invokeWebApi(cmd, jdata, call){
 //    $.getJSON(WebServerURL+cmd+"?callback=?", jdata, call);
