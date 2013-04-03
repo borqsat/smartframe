4dac27863685270f3b1638803caa11b24e476980
hongbin.bao <test@test-OptiPlex-740-Enhanced.(none)>
2013-04-03 10:45:21 +0800

    delete server dir

--- runner/stability/__init__.py
+++
@@ -1,9 +0,0 @@
-from impl.builder import TestBuilder
-from impl.application import Application
-from impl.devicemanager import DeviceManager
-from impl.testcasebase import TestCaseBase
-from impl import listener
-from util.log import Logger
-
-__all__ = ['TestBuilder','Application','DeviceManager','TestCaseBase','listener','Logger']
-
