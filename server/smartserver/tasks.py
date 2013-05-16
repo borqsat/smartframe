710623acd72468643a3ad6aee47ce110dac94cee
shaofang <shaofang.zhang@gmail.com>
2013-05-16 14:20:12 +0800

    Implement clear dirty and tests

--- server/smartserver/tasks.py
+++ server/smartserver/tasks.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from .worker import worker as w
 
 from .v0.impl.dbstore import store
-from bson.objectid import ObjectId
 
 @w.task
 def add(x, y):
@@ -20,30 +19,26 @@ def xsum(numbers):
     return sum(numbers)
 
 @w.task
-def delSessionFs(sid):
+def ws_del_session(sid):
     '''
     Delete FS according to the sid by using worker task
     '''
-    caseresult = store._db['testresults']
-    crs = caseresult.find({'sid':sid, 'result':'fail'})
-    for record in crs:
-        if 'snapshots' in record:
-            snapshots = record['snapshots']
-            for snap in snapshots: store.deletefile(snap['fid'])
-            
-        if 'checksnap' in record:
-            checksnap = record['checksnap']
-            store.deletefile(checksnap['fid'])
-            store.deletefile(record['log'])
+    store.del_session(sid)
 
 @w.task
-def delGroupFs(gid):
+def ws_del_group(gid):
     '''
     Delete FS and results according to gid by using worker task
     '''
+    store.del_group(gid)
 
 @w.task
-def delDirtyFs():
+def ws_del_dirty():
     '''
     Scheduled task to clear dirty FS.
     '''
+    store.del_dirty()
+
+@w.task
+def ws_check_fs(fid):
+	store.check_fs(fid)
